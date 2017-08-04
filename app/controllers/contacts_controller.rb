bcab076fb163b1b413c80b8a7e22a5fdc6242815
Madhu <madhu.mishra20@gmail.com>
2017-08-04 17:02:16 +0000

    Added Hirb gem for data presentation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
-    def new
+  def new
+  @contact = Contact.new
+  # creates a blank object {name: , email: , comments: }
+  end
+  def create
+    @contact = Contact.new(contact_params) 
+    # {name:Madhu, email:madhu.mishra@gmail.com, comment:hi}
+    if @contact.save
+       redirect_to new_contact_path, notice: "Message sent."
+    else
+       redirect_to new_contact_path, notice: "Error occured."
+    end
+  end
+  private
+    def contact_params
+       params.require(:contact).permit(:name, :email, :comments)
     end
 end
