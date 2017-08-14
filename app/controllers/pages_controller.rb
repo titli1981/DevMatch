ab7ed6428d8ba4d77265caf45d68b013cfdcf09b
Madhu <madhu.mishra20@gmail.com>
2017-08-14 16:46:53 +0000

    Added conditional logic on sign up form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
-  def home
+  # Get request for '/' which is our hoe page
+  def home                    #     id    name   price   created_at
+    @basic_plan = Plan.find(1) #  (| 1  | basic | 0.0   | 2017-08-1... | 2017-08-1...)
+    @pro_plan = Plan.find(2) # (| 2  | pro  | 10.0  | 2017-08-1... | 2017-08-14... )
   end
   def about
   end
